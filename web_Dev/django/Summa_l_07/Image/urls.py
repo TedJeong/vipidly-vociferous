69634df18c9b011b24d7e500a1b7e008ee692d9c
TedJeong <TedJeong@naver.com>
2016-12-25 20:57:45 +0900

    desktop 201612252056 added

--- web_Dev/django/Summa_l_07/Image/urls.py
+++ web_Dev/django/Summa_l_07/Image/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import url
 
 
 from .views import index
+from .views import simple
 
 app_name = 'image'
 
 urlpatterns = [
     # /image/
     url(r'^$', index , name="index"),
+    url(r'^matplotlib-demo/$', simple, name="simple"),
 ]
